7c03a4d4c323fba566bac4107a4485a45bfe0939
Prilvesh Krishna <prilcool@gmail.com>
2021-02-22 04:20:46 +1200

    Add files via upload

--- lab/simple_interest_audit.cpp
+++ lab/simple_interest_audit.cpp
@@ -9,58 +9,125 @@
 // =============================================================
 
 
+// Step 1 Include necessary headers and name spaces 
 
-#include<CL/sycl.hpp>
-#include<array>
-#include<iostream>
-#include<cmath>
+#include <CL/sycl.hpp>
+#include <array>
+#include <iostream>
+#include <cmath>
 #include<iomanip>
 #include<limits>
 #include <chrono>
 
-
-
 using namespace sycl;
 
+//Step 2  in the main program
+
 int main() {
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    int N = 5;
-    queue q(cpu_selector{});
-    std::cout << "Device: " << q.get_device().get_info<info::device::name>() << std::endl;
-    double*data = malloc_shared<double>(N, q);
-    double bank_balances[]={100.95,25,0.50,5.99};
-    double*bank_interest_rates= malloc_shared<double>(N, q);
-    double*accumulated_rates=malloc_shared<double>(N, q);
-    for (int i =0; i < 5; i++) {i=bank_balances[i];}
-    
-    std::cout << "Parallel data processing initialized" << std::endl;
-    q.parallel_for(range<1>(N), [=](id<1> i) {
-        data[i]=bank_balances[i]+2.00; 
-        bank_interest_rates[i]=data[i]*0.20;
-        accumulated_rates[0]=accumulated_rates[0]+bank_interest_rates[i];
-        
-    }).wait();
-
-    std::cout << "Proceeding to output results to csv file " << std::endl;
-    std::ofstream out("parallel_interest_on_account_audit.txt", std::ios::app);
-    
-   
-    out << "Record ID,Original Balance,Addon Interest "<< std::endl; 
-    for (int i = 0; i < N; i++) {
-    out << i << "," << std::fixed << std::setprecision(2) << data[i] << "," << bank_interest_rates[i] << std::endl;
-    }
   
-     std::cout << "Processing complete you can now refer to the audit file on the left hand side named parallel_interest_on_account_audit.csv " << std::fixed << std::setprecision(2) << accumulated_rates[0] << std::endl;
-     auto current_time = std::chrono::high_resolution_clock::now();
 
-     std::cout << "Processing completed in  " << std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count() << " seconds" << std::endl;
+    // Declare start time  as we want to track how long it takes  for our program to run.
+    
+     auto start_time = std::chrono::high_resolution_clock::now();
+    
+    
+    //Declare interger to specify number of records to process  our size is  1 million
+    
+    
+    int N =1000000;
+    
+    //Declare Interest rates  that you want to apply to an bank account 
+    double interest_rate_1=0.01;
+    double interest_rate_2=0.02;
+    double interest_rate_3=0.03;
+    double interest_rate_4=0.04;
+    double interest_rate_5=0.05;
+    double interest_rate_6=0.07;
+  
+    //Define  the device that you want to use for processing in our case  CPU   
+      // if you select a GPU device than Device: Intel(R) Graphics Gen9 [0x3e96] will process in 5 second.
+     queue q(gpu_selector{});
+    
+    //if you select a CPU device than Device: Intel(R) Xeon(R) E-2176G CPU @ 3.70GHz will process in 5 seconds.
+    //to use cpu un comment the below and comment the above.
+    //queue q(cpu_selector{});
+    
+    //Show on screen what device was selected 
+    
+    std::cout << "Device: " << q.get_device().get_info<info::device::name>() << std::endl;
+    //Allocate the  shared memory space to your  arrays 
+    
+    double* data = malloc_shared<double>(N, q);
+    double* bank_interest_rates = malloc_shared<double>(N, q);
+    double* rate_used= malloc_shared<double>(N, q);
 
-     free(data, q);
-     free(bank_interest_rates, q);
-     free(accumulated_rates, q);
-    return 0;
+//Step 3  iterate and allocate valeus from  1 to 1 million to I
+    
+    for (int i = 1; i < N; i++) {
+        i = i ;
+    }
+    
+    
+// Begin paralelle processing using the CPU device 
+    
+        q.parallel_for(range<1>(N), [=](id<1> i) {
+    
+            
+ // Enter our conditional interest rate  calculation alogrithm .
+            
+            double apply_interest_rate=0.00;
+            data[i] = (double)i + 0.00;
+            
+            if(i >= 1 && i <= 50000){
+               apply_interest_rate=interest_rate_1; 
+            }else if(i >=50001 && i <= 100000){
+               apply_interest_rate=interest_rate_2; 
+            }else if(i >=100001 && i <= 400000){
+               apply_interest_rate=interest_rate_3; 
+            }else if(i >=400001 && i <= 700000){
+               apply_interest_rate=interest_rate_4; 
+            }else if (i >=700001 && i <= 1000000){
+               apply_interest_rate=interest_rate_5; 
+            }else if (i >=1000001){
+               apply_interest_rate=interest_rate_6; 
+            }
+            else{
+               apply_interest_rate=0.00; 
+            }
+            
+            
+            bank_interest_rates[i] =(double)i* apply_interest_rate;
+            rate_used[i]=apply_interest_rate;
+            }).wait();
+
+    
+    
+    //Step 4  Notify user about whats happening and write file parralel_one_million_records_for_audit.txt  to disk 
+
+            std::cout << "Writing Computations to file " << std::endl;
+    
+            std::ofstream out("parralel_one_million_records_for_audit.txt", std::ios::app);
+
+            for (int i = 1; i < N; i++) {
+                out << "Record ID:00" << i << " Original Balance: " << std::fixed << std::setprecision(2) << data[i] << " Addon Interest: " << bank_interest_rates[i] 
+                <<" Interest Rate used %:" <<rate_used[i]<<std::endl;
+            }
+
+   
+           std::cout << "Processing complete you can now refer to the audit file on the left hand side named parallel_interest_on_account_audit.txt " << std::endl;
+           auto current_time = std::chrono::high_resolution_clock::now();
+
+           std::cout << "Processing completed in  " << std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count() << " seconds" << std::endl;
+    
+    //  Release the shared memory space  that you allocated earlier.
+    
+            free(data, q);
+            free(bank_interest_rates, q);
+            free(rate_used, q);
+    
+            return 0;
 }
+
 
 
 
